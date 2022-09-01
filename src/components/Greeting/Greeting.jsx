5601960c45709337e48abbd7a0467f58887b83f1
Chhattoo25 <95302680+Chhattoo25@users.noreply.github.com>
2022-09-01 17:12:29 +0530

    some changes and change link

--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -1,37 +1,39 @@
-import React, { useState } from 'react'
-import styles from  './Greeting.module.css'
+import React, { useState } from "react";
+import styles from "./Greeting.module.css";
 const Greeting = () => {
+  let data = new Date();
+  data = data.getHours();
+  let time = new Date().toLocaleDateString();
+  let time2 = new Date().toLocaleTimeString();
+  let greeting = "";
 
- let data = new Date(2022,8,31,1)
-   data = data.getHours()
- let time = new Date().toLocaleDateString() 
- let time2 = new Date().toLocaleTimeString()
- let greeting = ''
+  const cssStyle = {};
 
-  const cssStyle = {}
-
- if(data>=1 && data<12){
-  
-  greeting = 'Good morning'
-  cssStyle.color= 'green'
- }
- else if(data>=12 && data<20) {
-  greeting = 'Good Afternoon'
-  cssStyle.color = 'orange'
- }
- else{
- greeting = "Good Night"
- cssStyle.color = 'blue'
- }
+  if (data >= 1 && data < 12) {
+    greeting = "Good morning";
+    cssStyle.color = "green";
+  } else if (data >= 12 && data < 20) {
+    greeting = "Good Afternoon";
+    cssStyle.color = "orange";
+  } else {
+    greeting = "Good Night";
+    cssStyle.color = "blue";
+  }
   return (
     <>
-   <div className={styles.box}>
-   <h1 className={styles.greet} >Hello sir, <span style={cssStyle} >{greeting}</span>  </h1>
-    <h4 ><span className={styles.date}>Date</span> {`: ${time}`}</h4>
-    <h4 ><span className={styles.date}>Time</span> {`: ${time2}`}</h4>
-   </div>
+      <div className={styles.box}>
+        <h1 className={styles.greet}>
+          Hello sir, <span style={cssStyle}>{greeting}</span>{" "}
+        </h1>
+        <h4>
+          <span className={styles.date}>Date</span> {`: ${time}`}
+        </h4>
+        <h4>
+          <span className={styles.date}>Time</span> {`: ${time2}`}
+        </h4>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Greeting
+export default Greeting;
